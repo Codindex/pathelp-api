6cf75bcd61d0ac3483a006920279b3752d8fd3c3
Codindex <61005232+Codindex@users.noreply.github.com>
2023-08-13 17:55:23 +0200

    add activation option

--- models/category_owner.js
+++ models/category_owner.js
@@ -1,5 +1,10 @@
 const Sequelize = require("sequelize");
 const db = require("../db.js");
 
-const Category_Owner = db.define('category_owner', {});
+const Category_Owner = db.define('category_owner', {
+    is_available: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    }
+});
 module.exports = Category_Owner;
--- models/point_owner.js
+++ models/point_owner.js
@@ -1,5 +1,10 @@
 const Sequelize = require("sequelize");
 const db = require("../db.js");
 
-const Point_Owner = db.define('point_owner', {});
+const Point_Owner = db.define('point_owner', {
+    is_available: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    }
+});
 module.exports = Point_Owner;
